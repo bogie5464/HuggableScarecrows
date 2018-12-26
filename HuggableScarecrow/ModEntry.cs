81f20dfcc1666a93cbe1e021c7288f176ae79cf9
bogie5464 <bogard5150@gmail.com>
2018-12-25 21:41:52 -0500

    Removed debugging logs
    Required for polished finish.

--- HuggableScarecrow/ModEntry.cs
+++ HuggableScarecrow/ModEntry.cs
@@ -1,15 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 using StardewModdingAPI;
 using StardewValley;
 using StardewValley.Buildings;
-using StardewValley.Objects;
-using StardewValley.Locations;
 
-namespace HuggableScarecrow
+namespace HuggableScarecrows
 {
     public class ModEntry : Mod
     {
@@ -20,25 +14,18 @@ namespace HuggableScarecrow
 
         private void GameLoop_DayStarted(object sender, StardewModdingAPI.Events.DayStartedEventArgs e)
         {
-            this.Monitor.Log("Starting Day, checking buildings");
             foreach (Building building in Game1.getFarm().buildings)
             {
                 this.Monitor.Log($"Checking Building {building.buildingType.Value}");
                 if (building is Coop || building is Barn)
                 {
-                    this.Monitor.Log("Building was a coop/barn moving on");
-                    //var getIndoors = Helper.Reflection.GetMethod(building, "getIndoors");
-                    //GameLocation bldgIndoors = getIndoors.Invoke<GameLocation>(building.nameOfIndoorsWithoutUnique);
                     AnimalHouse indoors = building.indoors.Value as AnimalHouse;
                     foreach (StardewValley.Object furniture in indoors.Objects.Values)
                     {
-                        this.Monitor.Log($"Checking object {furniture.Name}");
                         if (furniture.Name.Contains("arecrow"))
                         {
-                            this.Monitor.Log("Building Contained Scarecrow");
                             foreach (KeyValuePair<long, FarmAnimal> animal in indoors.animals.Pairs)
                             {
-                                this.Monitor.Log("Found animals");
                                 animal.Value.pet(Game1.MasterPlayer);
                                 this.Monitor.Log("Pet animal");
                             }
